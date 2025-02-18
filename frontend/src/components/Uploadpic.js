321a3f22ccd2a86a48d2aa6904da0a5f4235110d
Aman <128085412+amannpanchal@users.noreply.github.com>
2025-02-18 20:22:29 +0530

    Update Uploadpic.js

--- frontend/src/components/Uploadpic.js
+++ frontend/src/components/Uploadpic.js
@@ -1,92 +1,86 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addpic } from '../redux/actions';
 
 const Uploadpic = () => {
     const dispatch = useDispatch();
-    const [picture,setPicture] = useState('');
-    const [pic , setPic] = useState('');
-    const [title , setTitle] = useState('');
-    const id  = localStorage.getItem('id')
-    const uploadPic = async(e)=> {
+    const [file, setFile] = useState(null);
+    const [mediaUrl, setMediaUrl] = useState('');
+    const [title, setTitle] = useState('');
+    const id = localStorage.getItem('id');
+
+    const uploadMedia = async (selectedFile) => {
         try {
-            console.log("hello")
-            if (picture.type === "image/jpeg" || picture.type === "image/jpg" || picture.type === "image/png"){
-                console.log("The ")
-                const data = new FormData();
-                data.append('file',picture);
-                data.append('upload_preset',"amanpanchal");
-                data.append('cloud_name','dk2scs5jz');
-                await fetch('https://api.cloudinary.com/v1_1/dk2scs5jz/image/upload', {
-                    method: 'POST',
-                    body: data
-                }).then(response => response.json())
-                    .then((data)  => {
-                       
-                       setPic(data.secure_url)
-                       console.log("the pic is ",pic)
-                    }
-                    )
-                    .catch(error => console.error(error))
+            if (!selectedFile) {
+                alert("Please select a file");
+                return;
             }
-            else {
-                alert("The image is not of image type")
+
+            const isImage = selectedFile.type.startsWith("image/");
+            const isVideo = selectedFile.type.startsWith("video/");
+
+            if (!isImage && !isVideo) {
+                alert("Only images and videos are allowed!");
+                return;
             }
-    
-    
-        }catch(e) {
-            console.log("The error in the request is ",e);
+
+            const data = new FormData();
+            data.append('file', selectedFile);
+            data.append('upload_preset', "amanpanchal");
+            data.append('cloud_name', 'dk2scs5jz');
+
+            const uploadUrl = isImage
+                ? 'https://api.cloudinary.com/v1_1/dk2scs5jz/image/upload'
+                : 'https://api.cloudinary.com/v1_1/dk2scs5jz/video/upload';
+
+            const response = await fetch(uploadUrl, {
+                method: 'POST',
+                body: data
+            });
+
+            const result = await response.json();
+            setMediaUrl(result.secure_url);
+            console.log("Uploaded file URL:", result.secure_url);
+        } catch (e) {
+            console.error("Error uploading media:", e);
         }
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (title && mediaUrl) {
+            const data = { title, pic: mediaUrl, id };
+            console.log("Submitting data:", data);
+            dispatch(addpic(data));
+        } else {
+            alert("Please add a title and upload a media file");
         }
+    };
 
-
-console.log("The pic is :",pic);
-const onSubmit = (e) => {
-    e.preventDefault();
-  if(title && pic) {
-    const data = {
-        title,
-        pic ,id
-    }
-    console.log(data);
-    dispatch(addpic(data))
-
-  }else if(!title) {
-    alert(
-        "Enter image title"
-    )
-  }
-  else if(!pic) {
-alert("Submit your pic ")
-  }
-  else {
-    alert("Add image and title")
-  }
-    
-}
-   
     return (
         <div>
-
             <div className='inputside'>
-                <input type='text' value={title} placeholder='Enter image title...' onChange={(e) => {
-                    setTitle(e.target.value);
-                }}
-                /> <br />
-                <input type='file'
-                    accept='image/*'
-                    onChange={ (e) => {
-                        setPicture(e.target.files[0]);
-                 uploadPic()
+                <input
+                    type='text'
+                    value={title}
+                    placeholder='Enter media title...'
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <br />
+                <input
+                    type='file'
+                    accept='image/*,video/*'
+                    onChange={(e) => {
+                        const selectedFile = e.target.files[0];
+                        setFile(selectedFile);
+                        uploadMedia(selectedFile);
                     }}
                 />
                 <br />
-                <button className='upload' onClick = {onSubmit}>Upload</button>
-
-
+                <button className='upload' onClick={onSubmit}>Upload</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Uploadpic
+export default Uploadpic;
